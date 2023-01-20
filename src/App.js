3fdb68b78318f34f5616255a3d28a22e99e74b5d
Chelsea <chelseaeverest7@gmail.com>
2023-01-20 16:17:33 -0500

    Adding projects page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
+import Projects from "./pages/Projects";
 import NoPage from "./pages/NoPage";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="/about" element={<AboutMe />}/>
+            <Route path="/projects" element={<Projects />}/>
             <Route path="*" element={<NoPage />} />
           </Route>
         </Routes>
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -8,17 +8,25 @@ function Layout() {
   const navigateAboutMe = () =>{ 
     navigate("/about");
   }
+  const navigateProjects = () =>{ 
+    navigate("/projects");
+  }
   return (
     <>
       <nav>
-        <ul>
-          <li>
-            <button onClick={navigateHome}>Home</button>
-          </li>
-          <li>
-            <button onClick={navigateAboutMe}>About Me</button>
-          </li>
-        </ul>
+        <table>
+          <tr>
+            <td>
+              <button onClick={navigateHome}>Home</button>
+            </td>
+            <td>
+              <button onClick={navigateAboutMe}>About Me</button>
+            </td>
+            <td>
+              <button onClick={navigateProjects}>Projects</button>
+            </td>
+          </tr>
+        </table>
       </nav>
 
       <Outlet />
