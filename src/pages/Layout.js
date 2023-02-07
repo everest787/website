ac1c5ed731ee361d5cc482b58bfe121f195fd773
Chelsea <chelseaeverest7@gmail.com>
2023-02-07 18:33:10 -0500

    Adding the initial homepage layout

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -21,16 +21,23 @@ function Layout() {
         </div>
         <button className="header__contact">Contact Me</button>
       </header>
+      <main className="personal">
+        <h1 className="personal__name">
+          Chelsea Everest
+        </h1>
+        <figure className="personal__item">
+          <img src={snowmanPicture} alt="Snowman and I" className="personal__photo"/>
+        </figure>
+      </main>
       <div className="content">
-        <main className="personal">
-          <h1 className="personal__name">
-            Chelsea Everest
-          </h1>
-          <figure className="personal__photo">
-            <img src={snowmanPicture} alt="Snowman and I" className="main__photo"/>
-          </figure>
-        </main>
-
+        <div className="content__about-me">
+          About Me
+        </div>
+        <div className="content__pitch">
+          <p className="content__pitch-paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed urna iaculis, pretium mauris id, imperdiet diam. Donec consectetur interdum libero eget ornare. Nunc volutpat fermentum elementum. Fusce dignissim aliquet enim et ultrices. Praesent venenatis non magna at venenatis. Pellentesque ut diam porttitor, mollis quam et, ultricies lectus. Maecenas cursus justo sit amet vehicula vulputate.
+          </p>
+        </div>
       </div>
     </div>
       <nav>
