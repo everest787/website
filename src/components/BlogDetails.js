24b0fe0672bb2eda8e364b0fb51d6ac24544f66c
Chelsea <chelseaeverest7@gmail.com>
2023-02-16 16:54:25 -0500

    Basic styles for the blogs

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,10 +1,10 @@
 function BlogDetails({ blog }) { 
     return (  
         <div className="blog-page__blog-details">
-            <h1 className="blog-page__blog-details__heading">{blog.title}</h1>
             <img className="blog-page__blog-details__img" src={blog.imgUrl} alt="post" />
-            <p className="blog-page__blog-details__content">{blog.body}</p>
+            <h1 className="blog-page__blog-details__heading">{blog.title}</h1>
             <h3 className="blog-page__blog-details__date">{blog.date}</h3>
+            <p className="blog-page__blog-details__content">{blog.body}</p>
         </div>
     );
 }
