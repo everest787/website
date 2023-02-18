32d7530a14ae5095779860dabaea6077a0b8ef1e
Chelsea <chelseaeverest7@gmail.com>
2023-02-18 14:07:17 -0500

    Styling and creating flippable cards for blog

--- src/components/BlogThumbnail.js
+++ src/components/BlogThumbnail.js
@@ -1,9 +1,14 @@
 function BlogThumbnail({ blog }) { 
     return (  
         <div className="blog__thumbnail">
-            <img className="blog__thumbnail__img" src={blog.imgUrl} alt="post" />
-            <h2 className="blog__thumbnail__heading">{blog.title}</h2>
-            <h3 className="blog__thumbnail__date">{blog.date}</h3>
+            <div className="blog__thumbnail__side blog__thumbnail__side--front">
+                <img className="blog__thumbnail__side--front__img" src={blog.imgUrl} alt="post" />
+                <h2 className="blog__thumbnail__side--front__heading">{blog.title}</h2>
+                <h3 className="blog__thumbnail__side--front__date">{blog.date}</h3>
+            </div>
+            <div className="blog__thumbnail__side blog__thumbnail__side--back">
+                <p className="blog__thumbnail__side--back__body">{blog.body}</p>
+            </div>
         </div>
     );
 }
