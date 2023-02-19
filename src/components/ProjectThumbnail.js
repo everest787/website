4c5bb4bd26599845842bee2685195c5eacfa17b1
Chelsea <chelseaeverest7@gmail.com>
2023-02-19 12:53:58 -0500

    Adding styles to project page plus backgrounds

--- src/components/ProjectThumbnail.js
+++ src/components/ProjectThumbnail.js
@@ -4,6 +4,7 @@ function ProjectThumbnail({ project }) {
             <h2 className="projects__thumbnail__heading">{project.title}</h2>
             <h3 className="projects__thumbnail__date">{project.date}</h3>
             <img className="projects__thumbnail__img" src={project.imgUrl} alt="post" />
+            <h3 className="projects__thumbnail__description">{project.desc}</h3>
         </div>
     );
 }
