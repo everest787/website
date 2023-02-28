e9f54cbc56d19bcd82efc26745827a6cb1a48fc4
Chelsea <chelseaeverest7@gmail.com>
2023-02-28 18:53:42 -0500

    Updating some Media Queries

--- src/components/ProjectThumbnail.js
+++ src/components/ProjectThumbnail.js
@@ -3,7 +3,9 @@ function ProjectThumbnail({ project }) {
         <div className="projects__thumbnail">
             <h2 className="projects__thumbnail__heading">{project.title}</h2>
             <img className="projects__thumbnail__img" src={project.imgUrl} alt="post" />
-            <h3 className="projects__thumbnail__description">{project.desc}</h3>
+            <div className="projects__thumbnail__desc-container">
+                <h3 className="projects__thumbnail__description">{project.desc}</h3>
+            </div>
             <div className="projects__thumbnail__footer">
                 <h3 className="projects__thumbnail__footer__key">{project.key}</h3>
                 <h3 className="projects__thumbnail__footer__date">{project.date}</h3>
